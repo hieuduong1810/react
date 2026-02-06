c4f5da14f0592429e65e8418ab08529588bb6e95
Hieuduong <hieuduong181005@gmail.com>
2026-02-06 16:11:48 +0700

    video 27

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+// () => { }
+const App = () => {
   const [count, setCount] = useState(0)
+
+  // function myFunction() {
+  //   console.log('This is a sample function')
+  // }
+
+  const myFunction = () => {
+    console.log('This is a sample function')
+  }
+
+  myFunction()
 
   return (
     <>
